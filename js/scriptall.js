410535d6b35f42366c13d8ab6226c58c30e8ac16
edrisso <jrd1sso@gmail.com>
2022-12-31 19:37:15 +0200

    update item.js

--- js/scriptall.js
+++ js/scriptall.js
@@ -44,17 +44,16 @@ function addItemToCartStorage(productId) {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-var items = [] ;
+var items = [];
 
+function furnitures() {
+  var furniture = products.filter(function (item) {
+    return item.category == "home-decoration";
+  });
 
-function furnitures(){
-var furniture = products.filter(function (item){
-  return item.category == "home-decoration"
-});
-
-var productContainer = document.getElementById("products_container");
-furniture.forEach(product => {
-  let item = `<a href="/item#${product.id}" class="nav-link"><div class="row bg-white pb-3 pt-3">
+  var productContainer = document.getElementById("products_container");
+  furniture.forEach((product) => {
+    let item = `<a href="/pages/item.html#${product.id}" class="nav-link"><div class="row bg-white pb-3 pt-3">
   <img src="${product.thumbnail}" alt="" class="col-lg-9 col-sm-12" style="height: 500px;">
   <div class="col-lg-3 col-sm-12  pt-2">
     <p class="pt-5" style="color: rgb(194, 167, 167);">FEATURED PRODUCT</p>
@@ -66,23 +65,23 @@ furniture.forEach(product => {
   </button></div>
 </div>
 <hr>
-</div></a>`  
-let prodId = document.querySelectorAll(".priceBtn").id ;
-productContainer.innerHTML += item ;
-});
-localStorageCart();
+</div></a>`;
+    let prodId = document.querySelectorAll(".priceBtn").id;
+    productContainer.innerHTML += item;
+  });
+  localStorageCart();
 }
 
-function Laptops(){
-  var laptop = products.filter(function (item){
-    return item.category == "laptops"
+function Laptops() {
+  var laptop = products.filter(function (item) {
+    return item.category == "laptops";
   });
   var productContainer = document.getElementById("products_container");
-  productContainer.addEventListener("click",function(){
-    console.log()
-  })
-  laptop.forEach(product => {
-    let item = `<a href="/item#${product.id}" class="nav-link">
+  productContainer.addEventListener("click", function () {
+    console.log();
+  });
+  laptop.forEach((product) => {
+    let item = `<a href="/pages/item.html#${product.id}" class="nav-link">
     <div class="row bg-white pb-3 pt-3">
     <img src="${product.thumbnail}" alt="" class="col-lg-9 col-sm-12" style="height: 500px;">
     <div class="col-lg-3 col-sm-12  pt-2">
@@ -94,20 +93,20 @@ function Laptops(){
     </button></div>
   </div>
   <hr>
-  </div></a>`
-  productContainer.innerHTML += item ;
+  </div></a>`;
+    productContainer.innerHTML += item;
   });
   localStorageCart();
-  }
+}
 
-function phone(){
-  var furniture = products.filter(function (item){
-    return item.category == "smartphones"
+function phone() {
+  var furniture = products.filter(function (item) {
+    return item.category == "smartphones";
   });
-  
+
   var productContainer = document.getElementById("products_container");
-  furniture.forEach(product => {
-    let item = `<a href="/item#${product.id}" class="nav-link"><div class="row bg-white pb-3 pt-3">
+  furniture.forEach((product) => {
+    let item = `<a href="/pages/item.html#${product.id}" class="nav-link"><div class="row bg-white pb-3 pt-3">
     <img src="${product.thumbnail}" alt="" class="col-lg-9 col-sm-12" style="height: 500px;">
     <div class="col-lg-3 col-sm-12  pt-2">
       <p class="pt-5" style="color: rgb(194, 167, 167);">FEATURED PRODUCT</p>
@@ -118,20 +117,20 @@ function phone(){
     </button></div>
   </div>
   <hr>
-  </div></a>`
-    productContainer.innerHTML += item ;
+  </div></a>`;
+    productContainer.innerHTML += item;
   });
   localStorageCart();
-  }
+}
 
-  function fragrances(){
-    var furniture = products.filter(function (item){
-      return item.category == "fragrances"
-    });
-    
-    var productContainer = document.getElementById("products_container");
-    furniture.forEach(product => {
-      let item = `<a href="/item#${product.id}"class="nav-link"><div class="row bg-white pb-3 pt-3">
+function fragrances() {
+  var furniture = products.filter(function (item) {
+    return item.category == "fragrances";
+  });
+
+  var productContainer = document.getElementById("products_container");
+  furniture.forEach((product) => {
+    let item = `<a href="/pages/item.html#${product.id}" class="nav-link"><div class="row bg-white pb-3 pt-3">
       <img src="${product.thumbnail}" alt="" class="col-lg-9 col-sm-12" style="height: 500px;">
       <div class="col-lg-3 col-sm-12  pt-2">
         <p class="pt-5" style="color: rgb(194, 167, 167);">FEATURED PRODUCT</p>
@@ -142,19 +141,19 @@ function phone(){
       </button></div>
     </div>
     <hr>
-    </div></a>`
-      productContainer.innerHTML += item ;
-    });
-    localStorageCart();
-    }
-    function skincare(){
-      var furniture = products.filter(function (item){
-        return item.category == "skincare"
-      });
-      
-      var productContainer = document.getElementById("products_container");
-      furniture.forEach(product => {
-        let item = `<a href="/item#${product.id}"class="nav-link"><div class="row bg-white ">
+    </div></a>`;
+    productContainer.innerHTML += item;
+  });
+  localStorageCart();
+}
+function skincare() {
+  var furniture = products.filter(function (item) {
+    return item.category == "skincare";
+  });
+
+  var productContainer = document.getElementById("products_container");
+  furniture.forEach((product) => {
+    let item = `<a href="/pages/item.html#${product.id}" class="nav-link"><div class="row bg-white ">
         <img src="${product.thumbnail}" alt="" class="col-lg-7 col-sm-12" style="height: 500px;">
         <div class="col-lg-3 col-sm-12  ">
           <p class="pt-5" style="color: rgb(194, 167, 167);">FEATURED PRODUCT</p>
@@ -165,27 +164,24 @@ function phone(){
         </button></div>
       </div>
       <hr>
-      </div></a>`
-        productContainer.innerHTML += item ;
-      });
-      localStorageCart();
-      }
-      
-      function localStorageCart() {
-
-        var priceBtn = document.querySelectorAll(".priceBtn");
- 
-for (var i of priceBtn){
-  i.addEventListener("click",function(event){
-    var productId = this.getAttribute('data-id') ;
-    var selectedProduct = products.find (function(item){
-      return item.id == productId
-    })
-    console.log(selectedProduct)
-    cart.push(selectedProduct);
-    localStorage.setItem("cart",JSON.stringify(cart))
-  })
-
+      </div></a>`;
+    productContainer.innerHTML += item;
+  });
+  localStorageCart();
 }
-      }
-      
+
+function localStorageCart() {
+  var priceBtn = document.querySelectorAll(".priceBtn");
+
+  for (var i of priceBtn) {
+    i.addEventListener("click", function (event) {
+      var productId = this.getAttribute("data-id");
+      var selectedProduct = products.find(function (item) {
+        return item.id == productId;
+      });
+      console.log(selectedProduct);
+      cart.push(selectedProduct);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    });
+  }
+}
