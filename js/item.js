cb01b92190d1ca7011c4b49777eb3acccbea1ebb
Abdelrahman Moussa <abdelrahmanmohamedabdelhameeed@gmail.com>
2022-12-31 18:42:31 +0200

    modi.

--- js/item.js
+++ js/item.js
@@ -1,9 +1,8 @@
-  let products
   let sProduct
   /*fetching data*/
   fetch("../js/json_data.json").then((Response)  =>  Response.json()).then(data => {
     let productId=1
-    products = data
+    let products = data
     sProduct = products.find((item) => {
         return item.id = productId
     }) 
@@ -41,6 +40,15 @@ document.querySelector(".content-container").innerHTML=htmlProduct
             localStorage.setItem("cart","[]");
             
           }
+          let cartItems = JSON.parse(localStorage.getItem("cart"))
+    
+          if(!cartItems.some (item => item.id == sProduct.id) ){
+
+            cartItems.push(sProduct)
+            localStorage.setItem("cart", JSON.stringify(cartItems));
+
+          }
+          else alert("Already exists")
 
         console.log(sProduct);
 
@@ -52,3 +60,21 @@ document.querySelector(".content-container").innerHTML=htmlProduct
 
      
 }
+/*esraa & abdalrhman
+function addItemToCartStorage(productId) {
+  let prod = products.find(function (item) {
+    return item.id == productId;
+  });
+
+  if (cart.length == 0) {
+    cart.push(prod);
+  } else {
+    let result = cart.find((element) => element?.id == productId);
+    if (result === undefined) {
+      cart.push(prod);
+    } else{
+      alert("you already purchased this product")
+    }
+  }
+  localStorage.setItem("cart", JSON.stringify(cart));
+}*/
