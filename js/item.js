263e501005f1585906ef3db4b55d6069bb2bc332
Abdelrahman Moussa <abdelrahmanmohamedabdelhameeed@gmail.com>
2022-12-31 19:36:37 +0200

    Editing

--- js/item.js
+++ js/item.js
@@ -1,13 +1,15 @@
-  let sProduct
-  /*fetching data*/
-  fetch("../js/json_data.json").then((Response)  =>  Response.json()).then(data => {
-    let productId=1
-    let products = data
+let sProduct;
+/*fetching data*/
+fetch("../js/json_data.json")
+  .then((Response) => Response.json())
+  .then((data) => {
+    let productId = 1;
+    let products = data;
     sProduct = products.find((item) => {
-        return item.id = productId
-    }) 
-    
-let htmlProduct = `<div class="text-container">
+      return (item.id = productId);
+    });
+
+    let htmlProduct = `<div class="text-container">
  
 <div class="allText">
 <h1> ${sProduct.title}</h1>
@@ -25,41 +27,40 @@ let htmlProduct = `<div class="text-container">
 </div>
 <img class="sneaker" src="${sProduct.thumbnail}">
 
-</div>`
-document.querySelector(".content-container").innerHTML=htmlProduct
-})
-  
-  
-  var count = 0;
-  var btn = document.getElementById("btn");
-  var disp = document.getElementById("display");
+</div>`;
+    document.querySelector(".content-container").innerHTML = htmlProduct;
+  });
 
-  document.querySelector(".content-container").onclick = function (e) {
-    if (e.target.classList.contains("btn")) {
-        if (!localStorage.getItem("cart")){
-            localStorage.setItem("cart","[]");
-            
-          }
-          let cartItems = JSON.parse(localStorage.getItem("cart"))
-    
-          if(!cartItems.some (item => item.id == sProduct.id) ){
+var count = 0;
+var btn = document.getElementById("btn");
+var disp = document.getElementById("display");
+let loginStorageId = sessionStorage.getItem("loggedUserId")
 
-            cartItems.push(sProduct)
-            localStorage.setItem("cart", JSON.stringify(cartItems));
 
-          }
-          else alert("Item already exists")
-
-        console.log(sProduct);
-
+document.querySelector(".content-container").onclick = function (e) {
+  if (e.target.classList.contains("btn")) {
+    if (!localStorage.getItem("cart")) {
+      localStorage.setItem("cart", "[]");
     }
-    
-    
-    //   count++;
-    //   disp.innerHTML = count;
+    let cartItems = JSON.parse(localStorage.getItem("cart"));
 
-     
-}
+    if (!cartItems.some((item) => item.id == sProduct.id)) {
+        if (loginStorageId) {
+            cartItems.push(sProduct);
+            localStorage.setItem("cart", JSON.stringify(cartItems));
+    
+        }else{
+            window.location.href = "../pages/signup.html";
+        }
+    } else alert("Item already exists");
+
+    console.log(sProduct);
+  }
+
+  //   count++;
+  //   disp.innerHTML = count;
+};
+
 /*esraa & abdalrhman
 function addItemToCartStorage(productId) {
   let prod = products.find(function (item) {
